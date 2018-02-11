c766500bc42724e0be4a602120ad124ffd350a4d
Brett Holman <holmanb0214@my.uwstout.edu>
2018-02-10 18:55:41 -0800

    adding single file read functionality, also made a *.h file for argument parsing

---
+++ args.h
@@ -0,0 +1,128 @@
+
+int get_args(int argc, char *argv[], char *directory);
+int validate_args(int argc, char *argv[]);
+int iterate_args(int argc, char *argv[], char *directory);
+int get_arg(char * arg, int init);
+
+// interpret CLI args
+int get_args_iter(int argc, char *argv[], char *directory){
+
+   int args=0;
+   
+   // if length == 1, no args passed 
+   if(argc == 1){
+	directory[0] = '.';
+	directory[1] = '\n';
+	return 0;
+   }
+    
+   // For this simple program, the only non-flag is the distination to ls
+   int directory_found=0;
+   for (int i=1;i<argc;i++){
+
+	// Searching for non-flag arg
+	if(argv[i][0] != '-'){
+		strcpy(directory, argv[i]); 	
+		directory_found=1;
+	}
+	// checking flags
+	else{
+	   	int j=1;
+		char character;
+		while(character=argv[i][j++]){
+			if(character=='l'){
+				args |= ARG_l;
+			//	printf("flag -l found\n");
+			}else if(character=='a'){
+				args |= ARG_a;
+			//	printf("flag -a found\n");
+			}else if(character=='R'){
+				args |= ARG_R;
+			//	printf("flag -R found\n");
+			}	
+		}
+	}
+   }
+   if(!directory_found){
+	directory[0]='.';
+	directory[1]='\0';
+   }
+   return args;
+} 
+
+
+
+// validate CLI args
+int validate_args(int argc, char *argv[]){
+	return 0;
+	// validate flags against list (alR)
+	// validate only one non-flag argument
+}
+
+
+
+int iterate_args(int argc, char *argv[], char *directory){
+
+	// Base case	
+	if(argc==1) return 0;
+	
+	// Don't get args if it is the directory 
+	//printf("argc:%d \n ", argc); 
+	//printf("argv:%s\n",  argv[argc-1]);
+	if(argv[argc-1][0] != '-'){
+		//printf("argv[argc-1][0]=%d\n", argv[argc-1][0]);
+		strcpy(directory, argv[argc-1]); 	
+		//printf("directory found in arglist: %s\n", directory);	
+		
+		// don't get the character for the directory
+		return iterate_args(--argc, argv, directory);
+	}
+	else{
+		return iterate_args(--argc, argv, directory) | get_arg(argv[argc], 1);	
+	}
+}	
+
+int get_arg(char * arg, int init){
+
+	// Searching for non-flag arg
+	// checking flags
+	int args=0;
+
+	// End of char* (base case)
+	if(!arg[init]) return 0;
+
+	// Check for args
+	if(arg[init]=='l'){
+		args |= ARG_l;
+		printf("flag -l found\n");
+	}else if(arg[init]=='a'){
+		args |= ARG_a;
+		printf("flag -a found\n");
+	}else if(arg[init]=='R'){
+		args |= ARG_R;
+		printf("flag -R found\n");
+	}	
+
+	// Check the other characters
+	return args | get_arg(arg, ++init);
+}
+
+// interpret CLI args
+int get_args(int argc, char *argv[], char *directory){
+
+   int args=0;
+   
+   // if length == 1, no args passed 
+   if(argc == 1){
+	directory[0] = '.';
+	return 0;
+   }
+    
+   // For this simple program, the only non-flag is the distination to ls
+   args = iterate_args(argc, argv, directory);
+   //printf("args found:%d", args);
+   if(!directory[0])
+	directory[0]='.';
+
+   return args;
+} 
--- ls.c
+++ ls.c
@@ -18,10 +18,11 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <errno.h>
+#include "args.h"
 
 void print_files(int num, struct dirent **namelist, int args, char *directory, char *buffer);
 int get_args(int argc, char *argv[], char *directory);
-int validate_args(int argc, char *argv[]);
+int validate_args(int argc, char *argv[]); // this is currently not implemented (TODO)
 int iterate_args(int argc, char *argv[], char *directory);
 int get_arg(char * arg, int init);
 int is_hidden_file(char *name);
@@ -51,9 +52,20 @@ int main(int argc, char *argv[])
    // do scandir and print out the directory
    n=scandir(directory, &namelist, NULL, alphasort); 
    if (n < 0){
-	printf("directory: %s\n", directory);
-       perror("scandir");
-   }else {
+
+	// possible a single file, try doing lstat on it
+	// currently doesn't handle a list of files, consider implementing this (TODO)
+	struct stat stats;
+	if(lstat(directory, &stats)==-1){
+
+		// This is not a filename or location, 
+		printf("%s\n", strerror(errno));
+		perror("scandir");
+		return -1;
+	}else{
+		printf("%u\t%s\n ", stats.st_size, directory);
+	}
+   }else{
 	printf("Directory listing of %s\n", directory);
 	printf("Size\tName\n");
 	print_files(n, namelist, args, directory,str_buffer);
@@ -82,7 +94,8 @@ void print_files(int num, struct dirent **namelist, int args, char *directory, c
 	// arg -a
 	int print_all = args&2;
 	if(print_all){
-		if(lstat(buffer, &stats)==1){
+		if(lstat(directory, &stats)==-1){
+		//if(!lstat(buffer, &stats)){
 			printf("%s\n", strerror(errno));
 		}
 		else{
@@ -91,7 +104,8 @@ void print_files(int num, struct dirent **namelist, int args, char *directory, c
 	}
 	// only print unhidden files 
 	else if(!hidden){
-		if(lstat(buffer, &stats)==1){
+		if(lstat(directory, &stats)==-1){
+		//if(!lstat(buffer, &stats)==1){
 			printf("%s\n", strerror(errno));
 		}
 		else{
@@ -116,124 +130,3 @@ int is_hidden_file(char *name){
 }
 
 
-// interpret CLI args
-int get_args_iter(int argc, char *argv[], char *directory){
-
-   int args=0;
-   
-   // if length == 1, no args passed 
-   if(argc == 1){
-	directory[0] = '.';
-	directory[1] = '\n';
-	return 0;
-   }
-    
-   // For this simple program, the only non-flag is the distination to ls
-   int directory_found=0;
-   for (int i=1;i<argc;i++){
-
-	// Searching for non-flag arg
-	if(argv[i][0] != '-'){
-		strcpy(directory, argv[i]); 	
-		directory_found=1;
-	}
-	// checking flags
-	else{
-	   	int j=1;
-		char character;
-		while(character=argv[i][j++]){
-			if(character=='l'){
-				args |= ARG_l;
-				printf("flag -l found\n");
-			}else if(character=='a'){
-				args |= ARG_a;
-				printf("flag -a found\n");
-			}else if(character=='R'){
-				args |= ARG_R;
-				printf("flag -R found\n");
-			}	
-		}
-	}
-   }
-   if(!directory_found){
-	directory[0]='.';
-	directory[1]='\0';
-   }
-   return args;
-} 
-
-
-// interpret CLI args
-int get_args(int argc, char *argv[], char *directory){
-
-   int args=0;
-   
-   // if length == 1, no args passed 
-   if(argc == 1){
-	directory[0] = '.';
-	return 0;
-   }
-    
-   // For this simple program, the only non-flag is the distination to ls
-   args = iterate_args(argc, argv, directory);
-   printf("args found:%d", args);
-   if(!directory[0])
-	directory[0]='.';
-
-   return args;
-} 
-
-// validate CLI args
-int validate_args(int argc, char *argv[]){
-	return 0;
-	// validate flags against list (alR)
-	// validate only one non-flag argument
-}
-
-
-
-int iterate_args(int argc, char *argv[], char *directory){
-
-	// Base case	
-	if(argc==1) return 0;
-	
-	// Don't get args if it is the directory 
-	//printf("argc:%d \n ", argc); 
-	//printf("argv:%s\n",  argv[argc-1]);
-	if(argv[argc-1][0] != '-'){
-		//printf("argv[argc-1][0]=%d\n", argv[argc-1][0]);
-		strcpy(directory, argv[argc-1]); 	
-		//printf("directory found in arglist: %s\n", directory);	
-		
-		// don't get the character for the directory
-		return iterate_args(--argc, argv, directory);
-	}
-	else{
-		return iterate_args(--argc, argv, directory) | get_arg(argv[argc], 1);	
-	}
-}	
-
-int get_arg(char * arg, int init){
-
-	// Searching for non-flag arg
-	// checking flags
-	int args=0;
-
-	// End of char* (base case)
-	if(!arg[init]) return 0;
-
-	// Check for args
-	if(arg[init]=='l'){
-		args |= ARG_l;
-		printf("flag -l found\n");
-	}else if(arg[init]=='a'){
-		args |= ARG_a;
-		printf("flag -a found\n");
-	}else if(arg[init]=='R'){
-		args |= ARG_R;
-		printf("flag -R found\n");
-	}	
-
-	// Check the other characters
-	return args | get_arg(arg, ++init);
-}
